775bdceeda99563f72537ab79e89682efc89d5cc
pashafaiz <mfaizpasha104@gmail.com>
2025-04-04 15:24:27 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import Navigator from './src/Navigation/Navigator'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Navigator />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
---
+++ src/afterLogin/signUp.tsx
@@ -0,0 +1,16 @@
+
+
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class signUp extends Component {
+    render() {
+        return (
+            <View>
+                <Text>signUp</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
