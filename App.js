2dc50de611445078971e79b201c9bdb797c19b65
Sarpu Khokhar <sarpukhokhar786@gmail.com>
2025-04-07 10:47:55 +0530

    Splash Screen1

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { Text, StyleSheet, View } from 'react-native'
 import React, { Component } from 'react'
 import Navigator from './src/Navigation/Navigator'
+// import Navigator from './src/Navigation/Navigator'
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Navigator />
+        <Navigator/>
       </View>
     )
   }
---
+++ src/Navigation/Navigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUp from '../afterLogin/signUp'; 
+import Splash from '../Screens/Splash';
+import DashBoard from '../Screens/DashBoard';
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="DashBoard" component={DashBoard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
